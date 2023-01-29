b3231ca0f37ae166e65d9e3188e2018c3fd29179
agoh kelechi <kellyblixz@gmail.com>
2023-01-29 08:12:43 +0100

    Footer responsiveness

--- src/Footer.js
+++ src/Footer.js
@@ -11,11 +11,11 @@ function Footer() {
   return (
     <div >
         <div className='footer w-full lg:h-600 h-64 lg:pb-0 pb-80'>
-            <h1 className='footer-text lg:text-6xl text-3xl text-center lg:font-semibold font-extrabold lg:ml-60 ml-10 lg:mr-64 mr-9 lg:pt-32 pt-20 '>Start learning with kredos academy</h1>
-            <h1 className='footer-subtext lg:text-xl text-xs font-medium lg:ml-72 ml-10 lg:mr-80 mr-10 text-center lg:font-normal'>A lot of course to explore including, frontend engeneering, backend engeneering amd data science</h1>
+            <h1 className='footer-text lg:text-6xl text-3xl text-center lg:font-semibold font-bold lg:ml-60 ml-10 lg:mr-64 mr-9 lg:pt-32 pt-20 '>Start learning with kredos academy</h1>
+            <h1 className='footer-subtext lg:text-xl text-sm font-medium lg:ml-72 ml-10 lg:mr-80 mr-10 text-center lg:font-normal'>A lot of course to explore including, frontend engeneering, backend engeneering amd data science</h1>
             <Link className="no-underline" /* to="/Application" */>
-            <div className="footer-button text-xs text-center font-semibold lg:w-40 w-28 lg:rounded-full rounded-3xl lg:h-16 h-10 lg:mt-6 mt-12 lg:ml-630 ml-32 no-underline">
-              <h1 className="lg:text-xl text-md text-center text-black lg:pt-4 pt-3 no-underline">Get Started</h1>
+            <div className="footer-button  text-center font-semibold lg:w-40 w-28 lg:rounded-full rounded-3xl lg:h-16 h-10 lg:mt-6 mt-12 lg:ml-630 ml-32 no-underline">
+              <h1 className="lg:text-xl text-md text-center text-black lg:pt-4 pt-2 no-underline">Get Started</h1>
             </div>
           </Link>
         </div>
