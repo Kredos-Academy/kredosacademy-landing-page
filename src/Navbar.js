18a1e0d86173e08de21eb1c4f24af5583c3deded
agoh kelechi <kellyblixz@gmail.com>
2023-01-30 09:17:23 +0100

    Application links

--- src/Navbar.js
+++ src/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
             Tuition
           </h1>
         </div> */}
-        <Link className="no-underline" /* to="/Application" */>
+        <Link className="no-underline"  to="/Application" >
           <div className="application-button text-xs font-semibold text-center lg:w-40 w-24 lg:rounded-full rounded-3xl lg:h-16 h-8 mt-6">
             <h1 className="lg:text-xl text-md text-center text-white lg:pt-4 pt-2 no-underline">Get Started</h1>
           </div>
