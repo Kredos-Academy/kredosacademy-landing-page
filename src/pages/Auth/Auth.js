7be1f7911e40fc0f127cd8f5855c1552a9ecc88f
izaakwalz <chukwumaizaak@gmail.com>
2023-02-01 15:33:03 +0100

    added hidden input

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './auth.css';
 import TextInput from './Input';
 import { Link } from 'react-router-dom';
 
 export default function Auth() {
+	const [bathId, setBatchId] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [showNext, setShowNext] = useState(false);
+
 	return (
 		<div className='auth p-20'>
 			<div className='w-full flex-none md:flex md:items-center md:justify-between'>
@@ -18,16 +23,38 @@ export default function Auth() {
 							className='w-full p-5 border border-[black] rounded-xl focus:outline-none focus:border-black mt-8'
 							name='fName'
 							placeholder='Batch no (e.g) - #0001'
+							// value={bathId}
+							onClick={(e) => setBatchId(e.target.value)}
 						/>
 						<TextInput
 							className='w-full p-5 border border-[black] rounded-xl focus:outline-none focus:border-black mt-8'
 							name='fName'
 							placeholder='password'
+							// value={password}
+							onClick={(e) => setPassword(e.target.value)}
 						/>
 
-						<button className='payment-button lg:w-10/12 w-6/12 lg:h-16 h-10 rounded-full lg:mb-16 mb-10 lg:ml-16 ml-20 mt-14'>
-							<h1 className='text-white lg:text-2xl text-md font-bold'>Sign up</h1>
-						</button>
+						{showNext === true ? (
+							<>
+								<TextInput
+									className='w-full p-5 border border-[black] rounded-xl focus:outline-none focus:border-black mt-8'
+									name='cPassword'
+									placeholder='confirm password'
+									// value={confirmPassword}
+									onClick={(e) => setConfirmPassword(e.target.value)}
+								/>
+								<button className='payment-button lg:w-10/12 w-6/12 lg:h-16 h-10 rounded-full lg:mb-16 mb-10 lg:ml-16 ml-20 mt-14'>
+									<h1 className='text-white lg:text-2xl text-md font-bold'>Sign up</h1>
+								</button>
+							</>
+						) : (
+							<button
+								className='payment-button lg:w-10/12 w-6/12 lg:h-16 h-10 rounded-full lg:mb-16 mb-10 lg:ml-16 ml-20 mt-14'
+								onClick={() => setShowNext(true)}
+							>
+								<h1 className='text-white lg:text-2xl text-md font-bold'>continue</h1>
+							</button>
+						)}
 
 						<div className='flex items-center justify-start flex-col my-4 md:flex-row'>
 							<span>Already have an account? </span>{' '}
